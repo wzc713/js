d0cd64a46c825f0cbd4e7460ce156b285b8174b2
wzc713 <13460317713@163.com>
2020-07-17 16:23:55 +0800

    11

---
+++ js练习/jquery/script/jquery.tablestytlet.js
@@ -0,0 +1,15 @@
+﻿; (function ($) {
+
+    //注册函数
+    $.fn.extend({
+        settablestyle:function () {
+            $('tr', this).mouseover(function () {
+                $(this).css('backgroundColor', 'yellow').siblings().css('backgroundColor', '');
+            });
+        },
+        cleartablestyle:function () {
+            $('tr', this).unbind().removeAttr('style');//移除所有时间，并初始化
+        }
+
+    });
+})(jQuery);
